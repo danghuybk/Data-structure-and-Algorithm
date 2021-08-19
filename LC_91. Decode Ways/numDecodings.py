e46b86b6318a111c4f2053b797a72eb28a51bde3
danghuybk <44634439+danghuybk@users.noreply.github.com>
2021-08-19 13:41:15 +0700

    Create numDecodings.py

---
+++ LC_91. Decode Ways/numDecodings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        N = len(s)
+        dp = [0] * (N) + [N]
+
+        for i in range (N - 1, -1, -1):
+            if s[i] != '0':
+                dp[i] += dp[i + 1]
+                if 10 <= int(s[i:i + 2]) <= 26 and i + 2 <= N:
+                    dp[i] += dp[i + 2]  
+        
+        return dp[0]
